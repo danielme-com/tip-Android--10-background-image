e73396ac8fc559101961f9a6752896bd673b5f74
danielme.com <danielme_com@yahoo.com>
2013-08-10 19:37:26 +0200

    first commit

---
+++ src/com/danielme/tipsandroid/background/MainActivity.java
@@ -0,0 +1,24 @@
+package com.danielme.tipsandroid.background;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.danieme.tipsandroid.background.R;
+
+
+/**
+ * 
+ * @author danielme.com
+ *
+ */
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}	
+
+}
